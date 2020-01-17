e3820339eb40938b73e01331d1a10cf2dd3bcf9b
hưng <nphbo1998@gmail.com>
2020-01-17 21:34:15 +0700

    add hover effect

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/directorty/directory.component'
+
+import './homepage.styles.scss'
+
+
+const Homepage = () => (
+    <div className='homepage' >
+       <Directory/>
+    </div>
+)
+export default Homepage
+
+
