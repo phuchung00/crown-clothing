f3a109dbee8baa70561498d84e805e8d5781871e
hưng <nphbo1998@gmail.com>
2020-03-06 23:46:07 +0700

    persist cart when reload page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Provider store = {store} >
+    <Provider store={store} >
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
